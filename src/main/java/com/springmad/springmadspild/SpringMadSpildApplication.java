d8cbf984220f52708ca13ef4fddc4ab2c93c3086
Omegz <omarmajed2000@hotmail.com>
2025-10-30 17:26:54 +0300

    chechlpoint

---
+++ src/main/java/com/springmad/springmadspild/SpringMadSpildApplication.java
@@ -0,0 +1,13 @@
+package com.springmad.springmadspild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMadSpildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMadSpildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springmad/springmadspild/SpringMadSpildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springmad.springmadspild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMadSpildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
